42ba2ec1fb957b93e1b56ac6191edb5eaa75b41c
Alina_Fitisova <alinafitisova@gmail.com>
2021-01-26 11:04:25 +0200

    finish main page

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,13 +15,23 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('webp');
     }
 });
-// burger function
+const docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+    document.querySelectorAll('*'),
+    function(el){
+        if (el.offsetWidth > docWidth) {
+            console.log(el);
+        }
+    }
+)
+
+// jQuery toggleBurger function
 ;$(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-
+// jQuery sliders function
     $('.header-slider').slick({
         dots: true,
         arrows : false,
@@ -42,9 +52,15 @@ testWebP(function (support) {
         infinite: true,
         lazyLoad: 'ondemand',
         speed: 1000,
-        cssEase: 'linear',
-        appendArrows: false,
-        
-        });
+        cssEase: 'linear'
+    });
+      // To up window button
+    $(window).on('scroll', function () {
+      if ($(this).scrollTop() > 500) {
+        $('.up-button').fadeIn();
+      } else {
+        $('.up-button').fadeOut();
+      }
+    });
 });
 
